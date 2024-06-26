46c5977f88d6dcf5e03eb45cabc47c27c3384896
VilaxDev <vilax.dev@gmail.com>
2024-06-26 18:15:40 -0500

    Implementacion de la funcionalidad del Boton aceptar en la pagina inventario negocio page.

--- lib/pages/modulo4/pedidos_negocio_page.dart
+++ lib/pages/modulo4/pedidos_negocio_page.dart
@@ -1,6 +1,7 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:pidenomas/pages/modulo4/agregar_producto_page.dart';
+import 'package:pidenomas/pages/modulo4/producto_negocio_page.dart';
 import 'package:pidenomas/ui/general/colors.dart';
 import 'package:pidenomas/ui/widgets/general_widgets.dart';
 import 'package:flutter/services.dart';
@@ -150,15 +151,16 @@ class PedidosNegocioPage extends StatelessWidget {
                       margin: EdgeInsets.all(10),
                       padding: EdgeInsets.all(10),
                       decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Colors.white,
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.black.withOpacity(0.4),
-                              offset: const Offset(4, 4),
-                              blurRadius: 7.0,
-                            ),
-                          ]),
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.4),
+                            offset: const Offset(4, 4),
+                            blurRadius: 7.0,
+                          ),
+                        ],
+                      ),
                       child: Row(
                         children: [
                           ClipRRect(
@@ -177,26 +179,35 @@ class PedidosNegocioPage extends StatelessWidget {
                               children: [
                                 Row(
                                   children: [
-                                    Text("S/${item['int1'].toString()}",
-                                        style: TextStyle(
-                                            fontSize: 16,
-                                            color: kBrandPrimaryColor2,
-                                            fontWeight: FontWeight.bold)),
+                                    Text(
+                                      "S/${item['int1'].toString()}",
+                                      style: TextStyle(
+                                        fontSize: 16,
+                                        color: kBrandPrimaryColor2,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
                                     SizedBox(width: 10.0),
-                                    Text("S/${item['int2'].toString()}",
-                                        style: TextStyle(
-                                            fontSize: 16,
-                                            color: Colors.grey,
-                                            fontWeight: FontWeight.w500,
-                                            decoration:
-                                                TextDecoration.lineThrough)),
+                                    Text(
+                                      "S/${item['int2'].toString()}",
+                                      style: TextStyle(
+                                        fontSize: 16,
+                                        color: Colors.grey,
+                                        fontWeight: FontWeight.w500,
+                                        decoration:
+                                            TextDecoration.lineThrough,
+                                      ),
+                                    ),
                                   ],
                                 ),
                                 SizedBox(height: 10),
-                                Text(item['name'],
-                                    style: TextStyle(
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.bold)),
+                                Text(
+                                  item['name'],
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
                               ],
                             ),
                           ),
